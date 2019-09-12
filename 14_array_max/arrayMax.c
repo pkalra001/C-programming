40a8e914162b1adefeef4ded5553ed7bda95bf9d
Coursera Learner <nobody@nowhere.nul>
2019-09-12 22:51:57 +0000

    Done

--- 14_array_max/arrayMax.c
+++ 14_array_max/arrayMax.c
@@ -2,7 +2,15 @@
 #include <stdlib.h>
 
 int * arrayMax(int * array, int n) {
-  return NULL;
+  if(n==0){
+    return NULL;}
+  int * m = &array[0];
+  for(int i = 0;i < n; i++){
+    if(array[i] > *m){
+      *m = array[i];}
+  }
+  return m;
+  
 }
 
 void doTest(int * array, int n) {
